112231d1085a3d076168c6dcb5e279d174032c04
Malkan <yuvalmalkan89@gmail.com>
2026-08-23 21:12:17 +0300

    tests for camera

--- main.py
+++ main.py
@@ -2,12 +2,34 @@ import cv2
 import numpy as np  
 import serial
 
+from Camera import Camera
+
 
 def main():
-    pass
+
+    camera = Camera(1)
+    
+    try:
+
+        camera.start()
+
+        while True:
+            success, frame = camera.readFrame()
+        
+            cv2.imshow('Live Camera Feed', frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("Exiting...")
+                break
+                
+    except RuntimeError as e:
+        print(e)
+        
+    finally:
+        camera.stop()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
     main()
 
-    

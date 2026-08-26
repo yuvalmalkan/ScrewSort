7cf04f199eb7be8fcd682e38f817683cb652fe91
Malkan <yuvalmalkan89@gmail.com>
2026-08-26 14:28:40 +0300

    cleanup

--- Constants.py
+++ Constants.py
@@ -1,26 +1,27 @@
-# ---- camera setup ----
+__author__ = "Yuval Malkan"
+
 CAPTURE_WIDTH_PX = 1280
 CAPTURE_HEIGHT_PX = 720
 
 
-# ---- region of interest (ROI) ----
+#region of interest
 ROI = (440, 160, 400, 400)
 
-# ---- detection tuning ----
+
 MIN_SCREW_AREA_PX = 500
-MAX_SCREW_AREA_PX = 18000  # Filters out large background machine parts
+MAX_SCREW_AREA_PX = 18000
 
 INVERT_THRESHOLD = True
 
-# Updated to 2.5 to filter out round reflections and non-screw objects
+#filter out round reflections
 MIN_LENGTH_TO_DIAMETER_RATIO = 2.5
 
 STABLE_FRAMES_REQUIRED = 10
 
-# Updated exact calibration for the laptop camera
+
 PIXELS_PER_MM = 5.8
 
-# ---- screw size classification ----
+#screw size classification
 DIAMETER_RANGES_MM = {
     "M1": (0.5, 1.5),
     "M2": (1.5, 2.5),
